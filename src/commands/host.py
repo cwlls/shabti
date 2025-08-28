543e7863189d6692cf808f6396e9580150ef087c
Chris Wells <hello@cwlls.com>
2025-08-28 17:33:34 -0400

    add a couple commands to test the hosts module

--- src/commands/host.py
+++ src/commands/host.py
@@ -11,18 +11,30 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 import click
 
+from tools import inventory_conf
 
-@click.group(help="commands to help find and manipulate hosts")
-def cli():
-    pass
-
-
-@click.command(help="find a specific host")
-def find():
-    click.echo("find a host")
+INVENTORY_FILE = inventory_conf.InventoryConf()
 
 
-cli.add_command(find)
+@click.group(help="commands to help find and manipulate hosts", invoke_without_command=True)
+@click.pass_context
+def cli(ctx):
+    if ctx.invoked_subcommand is None:
+        for host in INVENTORY_FILE.hosts.values():
+            if host.has_ip:
+                click.echo(f"{host.name}: {host.ip}")
+
+
+@cli.command()
+def ssh():
+    ## TEMP
+    user = "wells"
+    identity_file = "~/.ssh/id_rsa"
+    for host in INVENTORY_FILE.hosts.values():
+        click.echo(f"Host {host.name}")
+        click.echo(f"\tHostName {host.fqdn}")
+        click.echo(f"\tUser {user}")
+        click.echo(f"\tIdentityFile {identity_file}")
+        click.echo()
